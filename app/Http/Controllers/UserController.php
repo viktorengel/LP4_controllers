46a9cd3174875a59a454721b370d8c1783794bc6
viktorengel <viktorengel@hotmail.com>
2023-12-28 16:00:14 -0500

    Controlador apoyandose del modelo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(){
-        return view('user.index');
+        $users = User::all();
+        /* $notes = Notes::all(); */
+        return view('user.index',[
+            'users' => $users,
+            /* 'notes' => $notes */
+        ]);
     }
 
 }
