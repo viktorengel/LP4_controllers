0cbe93deced740b72e2dbe61196e22e80337ddc8
Viktor Rengel <viktorengel@hotmail.com>
2023-12-28 23:28:54 -0500

    Guardado de datos en DB

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index(){
-        $Users = User::all();
-        return view('user.index',compact('Users'));
+        $users = User::all();
+        return view('user.index',compact('users'));
+    }
+
+    public function create(){
+        $user = new User;
+        $user->name     = 'Viktor';
+        $user->email    = 'vik@rengel.com';
+        $user->password = Hash::make('12345');
+        $user->age      = 25;
+        $user->address  = 'Calle ML 363';
+        $user->zip_code = '254678';
+        $user->save();
+
+        User::create([
+            'name' => 'Emilia',
+            'email' => 'Emilia@rengel.com',
+            'password' => Hash::make('1234'),
+            'age' => '3',
+            'address' => 'Pusuqui',
+            'zip_code' => '246810'
+        ]);
+
+        User::create([
+            'name' => 'JuanD',
+            'email' => 'Juan@rengel.com',
+            'password' => Hash::make('123'),
+            'age' => '1',
+            'address' => 'Pusuqui C',
+            'zip_code' => '13579'
+        ]);
+
+        return redirect()->route('user.index');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class, 'index'])->name('user.index');
+
+Route::get('/create',[UserController::class, 'create'])->name('user.create');
